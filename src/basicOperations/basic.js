6e889e39bab181ea96067b84b8f356dc3e18aa6a
leotabosa <leeonardotabosa@gmail.com>
2021-01-18 19:32:35 -0300

    add tests

--- src/basicOperations/basic.js
+++ src/basicOperations/basic.js
@@ -6,10 +6,28 @@ export function subtract(a, b) {
   return a - b;
 }
 
-export function multiplicate(a, b) {
+export function multiply(a, b) {
   return a * b;
 }
 
 export function divide(a, b) {
   return a / b;
+}
+
+export function increase(a) {
+  let res = a;
+  res++;
+
+  return res;
+}
+
+export function decrease(a) {
+  let res = a;
+  res--;
+
+  return res;
+}
+
+export function modulus(a, b) {
+  return a % b;
 }
--- src/basicOperations/basic.test.js
+++ src/basicOperations/basic.test.js
@@ -1,4 +1,12 @@
-const { sum, subtract, divide, multiplicate } = require("./basic");
+const {
+  sum,
+  subtract,
+  divide,
+  multiply,
+  increase,
+  decrease,
+  modulus,
+} = require("./basic");
 
 describe("test basic operations", () => {
   it("sum - should sum numbers correctly", () => {
@@ -13,7 +21,19 @@ describe("test basic operations", () => {
     expect(divide(6, 2)).toBe(3);
   });
 
-  it("multiplicate - shoud multiplicate numbers correctly", () => {
-    expect(multiplicate(3, 2)).toBe(6);
+  it("multiply- shoud multiply numbers correctly", () => {
+    expect(multiply(3, 2)).toBe(6);
+  });
+
+  it("increase - should increase number", () => {
+    expect(increase(3)).toBe(4);
+  });
+
+  it("decrease - should decrease number", () => {
+    expect(decrease(4)).toBe(3);
+  });
+
+  it("modulus - should return remainder of two operands", () => {
+    expect(modulus(3, 2)).toBe(1);
   });
 });
